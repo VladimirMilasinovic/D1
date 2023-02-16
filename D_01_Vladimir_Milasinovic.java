3276ebf6765aa27a6f4aede3fbfd2e8900a1698c
VladimirMilasinovic <121833375+VladimirMilasinovic@users.noreply.github.com>
2023-02-16 20:24:14 +0100

    Add files via upload

---
+++ D_01_Vladimir_Milasinovic.java
@@ -0,0 +1,33 @@
+package Domaci;
+
+public class Domaciprvi {
+    public static void main(String[] args) {
+
+        String a;
+        String d;
+        String s;
+        String f;
+        Double g = 10.4;
+        Double h = 15.6;
+        Double j = 20.6;
+        Double k = 25.7;
+        a = "Jabuka";
+        s = "Kruska";
+        d = "Tresnja";
+        f = "Sljiva";
+
+
+        System.out.println("Cena jabuke bez popusta je: " + a);
+        System.out.println("Cena jabuke sa 30% popusta je: " + g/1.3);
+        System.out.println("Cena kruske bez popusta je: " + s);
+        System.out.println("Cena kruske sa 30% popusta je: " + h/1.3);
+        System.out.println("Cena tresnje bez popusta je: " + d);
+        System.out.println("Cena tresnje sa 30% popusta je: " + j/1.3);
+        System.out.println("Cena sljive bez popusta je: " + f);
+        System.out.println("Cena sljive sa 30% popusta je: " + k/1.3);
+
+
+
+    }
+
+}
